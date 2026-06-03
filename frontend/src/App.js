ab16259d9b1283840dba47a0ce3e6dc7a1124be0
thauamarcelo <thaua.marcelo.trabalhos@gmail.com>
2026-06-02 22:53:52 -0300

    arrumando rota DNVVVV

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -15,7 +15,7 @@ function App() {
         <Route path="/register" element={<Register />} />
         <Route path="/home" element={<Home />} />
         <Route path="/music" element={<Music/>}/>
-        <Route path="/movies" element={<Movies />} />
+        <Route path="/movies" element={<Movies/>} />
         <Route path="*" element={<Navigate to="/login" />} />
       </Routes>
     </BrowserRouter>
--- frontend/src/components/Movies.js
+++ frontend/src/components/Movies.js
@@ -2,7 +2,7 @@ import React, { useState, useEffect } from 'react';
 import { useNavigate, Link } from 'react-router-dom';
 import axios from 'axios';
 import './Home.css';
-import './Filme.css';  // Ou './Movies.css'
+import './Movies.css';  // Ou './Movies.css'
 
 const Movies = () => {
     const navigate = useNavigate();
